a9d67694916d30268c79417a7b88220e7c11e1ef
Christophe-Le-Laz <christophe.le-laz@oclock.school>
2023-12-07 16:21:30 +0100

    debut scroll chevron footer

--- app.js
+++ app.js
@@ -1,76 +1,109 @@
 const app = {
-    /**
-     * Propriétés
-     */
+  /**
+   * Propriétés
+   */
 
-    /**
-     * Méthodes
-     */
-  
-    init: function () {
-      // on attribut à notre const la valeur du bouton nouvelle tâche
-      
-      app.addClickProjectEvents();
-      app.showProject('projet1');
-  
-    },
-  
-    addClickProjectEvents: function () {
-        const liProjectElList = document.querySelectorAll('#left-zone li');
-        
-        for (const liProjectEl of liProjectElList) {
-            liProjectEl.addEventListener("click", app.handleDisplayProject);
-            
-        }
-      
-    },
-  
-    handleDisplayProject : function (event) {
-       
-        const itemValue = event.currentTarget.id;
-        // Cacher tous les projets
-        app.hideCurrentProject();
+  /**
+   * Méthodes
+   */
 
+  init: function () {
+    // on attribut à notre const la valeur du bouton nouvelle tâche
 
-        switch (itemValue) {
-            case 'p1':
-                app.showProject('projet1');
-                break;
-            case 'p2':
-                app.showProject('projet2');
-                break;
-            case 'p3':
-                app.showProject('projet3');
-                break;
-            case 'p4':
-                app.showProject('projet4');
-                break;
-            // Ajoutez d'autres cas au besoin
-        }
-    },
-  
+    app.addClickProjectEvents();
+    app.showProject("projet1");
+    app.initScrollButton();
 
-    hideCurrentProject: function () {
-        const projets = ['projet1', 'projet2', 'projet3', 'projet4'];
+  },
 
-        projets.forEach(projet => {
-            const projetElement = document.querySelector('.content_' + projet);
-            if (projetElement && projetElement.style.display !== 'none') {
-                projetElement.style.display = 'none';
-            }
-        });
-    },
+  addClickProjectEvents: function () {
+    const liProjectElList = document.querySelectorAll("#left-zone li");
 
-    showProject: function (projetId) {
-        const projetElement = document.querySelector('.content_' + projetId);
-        if (projetElement) {
-            projetElement.style.display = 'block';
-        }
+    for (const liProjectEl of liProjectElList) {
+      liProjectEl.addEventListener("click", app.handleDisplayProject);
     }
+  },
+
+  handleDisplayProject: function (event) {
+    const itemValue = event.currentTarget.id;
+    // Cacher tous les projets
+    app.hideCurrentProject();
+
+    switch (itemValue) {
+      case "p1":
+        app.showProject("projet1");
+        break;
+      case "p2":
+        app.showProject("projet2");
+        break;
+      case "p3":
+        app.showProject("projet3");
+        break;
+      case "p4":
+        app.showProject("projet4");
+        break;
+      // Ajoutez d'autres cas au besoin
+    }
+  },
+
+  hideCurrentProject: function () {
+    const projets = ["projet1", "projet2", "projet3", "projet4"];
+
+    projets.forEach((projet) => {
+      const projetElement = document.querySelector(".content_" + projet);
+      if (projetElement && projetElement.style.display !== "none") {
+        projetElement.style.display = "none";
+      }
+    });
+  },
+
+  showProject: function (projetId) {
+    const projetElement = document.querySelector(".content_" + projetId);
+    if (projetElement) {
+      projetElement.style.display = "block";
+    }
+  },
+  initScrollButton: function () {
+    const scrollButton = document.getElementById('scrollButton');
+    if (scrollButton) {
+        // Cacher le bouton au début
+        scrollButton.style.display = 'none';
+
+        // Récupérer la section avec l'id 'scene'
+        const sceneSection = document.querySelector('.name--technology');
+
+        // Variable pour suivre la direction du défilement
+        let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+
+        // Ajouter un écouteur d'événement pour le scroll
+        window.addEventListener('scroll', function () {
+            // Récupérer la position actuelle du défilement
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+            // Vérifier si la section 'scene' est visible dans la fenêtre
+            const sceneRect = sceneSection.getBoundingClientRect();
+            if (sceneRect.top < window.innerHeight && sceneRect.bottom >= 0) {
+                // Afficher le bouton si la section est visible et l'utilisateur défile vers le bas
+                if (scrollTop > lastScrollTop) {
+                    // Utiliser setTimeout pour retarder l'apparition du bouton
+                    setTimeout(function () {
+                        scrollButton.style.display = 'block';
+                    }, 500); // Ajouter un délai de 500 millisecondes (0.5 secondes)
+                }
+            } else {
+                // Cacher le bouton si la section n'est pas visible ou si l'utilisateur remonte
+                // Utiliser setTimeout pour retarder la disparition du bouton
+                setTimeout(function () {
+                    scrollButton.style.display = 'none';
+                }, 500); // Ajouter un délai de 500 millisecondes (0.5 secondes)
+            }
+
+            // Mettre à jour la position de défilement
+            lastScrollTop = scrollTop;
+        });
+    }
+},
 };
 
-
-
 // Dès que la page est complètement chargée, on exécute la méthode init rangée dans le "module" task
-document.addEventListener("DOMContentLoaded", app.init, );
-  
+document.addEventListener("DOMContentLoaded", app.init);
